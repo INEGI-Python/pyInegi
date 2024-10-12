129e4642811d7a3917852fae1fc88fa6e62ea196
Xino <emmanuel.rodriguez@inegi.org.mx>
2024-10-12 03:15:58 -0600

    funcion plot

--- pyInegi/basico.py
+++ pyInegi/basico.py
@@ -1,4 +1,16 @@
 from shapely import Point
+import folium
+import geopandas as gpd
+import os
+print(os.getcwd())
 
 def punto(x,y):
   return Point(x,y)
+
+def plot(gdf):
+  m = gdf.explore(column="id", cmap="Set1", name="id", legend=True, popup=True)
+  folium.TileLayer('OpenStreetMap').add_to(m)
+  folium.LayerControl().add_to(m)
+  m.show_in_browser()
+  
+
