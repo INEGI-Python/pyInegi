d5a2de6ddcdc7426cbb28679aa6559e1f6e0a757
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2024-10-14 09:21:54 -0600

    Archivo Generalizar.py y la clase reducePuntos.

--- pyInegi/__init__.py
+++ pyInegi/__init__.py
@@ -1,4 +1,5 @@
 from .basico import *
+from .generalizar import reducePuntos
 
 __version__="1.0.0"
 __ALL__ = ["basico"]
--- pyInegi/basico.py
+++ pyInegi/basico.py
@@ -2,7 +2,6 @@ from shapely import Point
 import folium
 import geopandas as gpd
 import os
-print(os.getcwd())
 
 def punto(x,y):
   return Point(x,y)
@@ -14,3 +13,17 @@ def plot(gdf):
   m.show_in_browser()
   
 
+def sepa(c="*-*-"):
+  print("%s" * 20  % tuple([c for i in range(20)]))
+
+def fechaHora():
+  import datetime as dt
+  t = dt.datetime.today()
+  return str(t)[:-7].replace(" ","-").replace(":","")
+
+def imp(text):
+  t = dt.datetime.today()
+  print("|%s|  %s" % (str(t)[5:-5],str(text)))
+
+def colores(v):
+  return "red" if v==True else "gray"
