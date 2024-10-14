0ad77c617e55b62a1a16d4202b88b956ebabda71
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2024-10-14 11:28:32 -0600

    se agregaron metodos basicos

--- pyInegi/basico.py
+++ pyInegi/basico.py
@@ -1,5 +1,7 @@
 from shapely import Point
 import folium
+import numpy as np
+import shapely as shpy
 import geopandas as gpd
 import os
 
@@ -12,6 +14,29 @@ def plot(gdf):
   folium.LayerControl().add_to(m)
   m.show_in_browser()
   
+
+
+def dist2pnts(x1, y1, x2, y2):
+	return ((x2-x1)**2+(y2-y1)**2)**0.5
+def importarSHP(shp):
+	import fiona
+	with fiona.open(shp) as shapefile:
+		for record in shapefile:
+			print(record)
+def shp2DF(shp,cuantos=None,campos=None):
+	return gpd.read_file(shp,rows=cuantos,columns=campos)
+def importFeat(gdb, feat, campos=None):
+	return gpd.read_file(gdb,layer=feat,columns=campos)
+def capa2Excel(gdb,feat,nom,campos=None):
+	tabla = gpd.read_file(gdb,layer=feat,columns=campos)
+	return tabla.to_excel(f"{nom}.xlsx")
+def capa2Excel(shp,nom,campos=None):
+	tabla = gpd.read_file(shp,columns=campos)
+	return tabla.to_excel(f"{nom}.xlsx")
+def pol2Linea(pol):
+	geo=list(pol.__geo_interface__['coordinates'][0])
+	return [shpy.LineString([shpy.Point(*list(geo[i-1])),shpy.Point(*list(geo[i]))]) for i in range(1,len(geo))]
+
 
 def sepa(c="*-*-"):
   print("%s" * 20  % tuple([c for i in range(20)]))
