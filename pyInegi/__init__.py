d1b995151a3b44c671197821e00db12c896209ab
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2024-10-23 14:04:46 -0600

    clase Generalizar

--- pyInegi/__init__.py
+++ pyInegi/__init__.py
@@ -1,10 +1,6 @@
-import generalizar as gen
+from .generalizar import Generalizar
 
-class Generalizar:
-    def reducePuntos(**_d):
-        gen.reducePuntos(gdb=_d.gdb, feat=_d.feat, camp=_d.camp.split(","),dist=_d.dist,ver=_d.ver)
-    def separaLineas(_gdb,_fCls,distancia,cpu=cpu_count(),vtx=0,eps=0.001,apx=0.02):
-        gen.separaLineas(*[_gdb,_fCls,distancia,cpu,vtx,eps,apx])
+
 
 __version__="1.0.0"
-__ALL__ = ["generalizar","basico","reducePuntos","separaLineas"]
+__ALL__ = ["Generalizar"]
