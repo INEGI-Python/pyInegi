3de61a74813cc644bad1835d93b39ffd7a1ad61f
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2024-10-23 14:29:12 -0600

    organizdo por categorias

--- pyInegi/generalizar.py
+++ pyInegi/generalizar.py
@@ -1,13 +1,3 @@
+from .basico import *
 from .reducePuntos import reducePuntos
 from .separaLineas import separaLineas
-from .basico import *
-
-class Generalizar:
-    def reducePuntos(**_d):
-        reducePuntos(gdb=_d.gdb, feat=_d.feat, camp=_d.camp.split(","),dist=_d.dist,ver=_d.ver)
-    def separaLineas(_gdb,_fCls,distancia,cpu=cpu_count(),vtx=0,eps=0.001,apx=0.02):
-        separaLineas(*[_gdb,_fCls,distancia,cpu,vtx,eps,apx])
-
-
-
-print(punto(5,9))
