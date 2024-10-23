6f43824d4024656fd4f81c3c2bfdf7f600bb0d79
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2024-10-23 11:15:44 -0600

    init externo vacio

--- pyInegi/__init__.py
+++
@@ -1,6 +0,0 @@
-from .generalizar.basico import *
-from .generalizar.reducePuntos import reducePuntos
-from .generalizar.separaLineas import separaLineas
-
-__version__="1.0.0"
-__ALL__ = ["basico","reducePuntos","separaLineas"]
---
+++ pyInegi/generalizar/__init__.py
@@ -0,0 +1,6 @@
+from .basico import *
+from .reducePuntos import reducePuntos
+from .separaLineas import separaLineas
+
+__version__="1.0.0"
+__ALL__ = ["basico","reducePuntos","separaLineas"]
