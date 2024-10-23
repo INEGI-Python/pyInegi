e3f9cb8172d3186a29de2703f0532b729d8f096c
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2024-10-23 16:04:59 -0600

    retorno de la clase geografia mas eficifente

--- pyInegi/geografia.py
+++ pyInegi/geografia.py
@@ -1,9 +1,14 @@
-def generalizar(func=""):
+def generalizar(func="ayuda"):
+	def ayuda():
+		return """ Aqui va la super ayuda de como usar la libreria 
+usar las lineas que le sean posibles"""
+	def desc():
+		return """pyInegi en su modulo generalizar fue desarrollado por el departamento de Generalización del Instituto Nacional de Estadisitica y Geografia. CopyRight   INEGI@2024 """
 	def _reduceP():
 		from .reducePuntos import inicio_rp
 		return inicio_rp
 	def _separaL():
 		from .separaLineas import inicio_sl
 		return inicio_sl
-	obj = {"reducePuntos":_reduceP(),"separaLineas":_separaL(),"ayuda":"Aqui va algunas presiciones del uso de esta libreria","desc":"Esta libreria fue desarrollada por el departamento de Generalización adscrito a la subdireccion de Edición Digital del INEGI. CopyRight INEGI@2024"}
-	return obj[func]
+	obj = {"reducePuntos":"_reduceP","separaLineas":"_separaL","ayuda":"ayuda","desc":"desc"}
+	return eval(obj[func])()
