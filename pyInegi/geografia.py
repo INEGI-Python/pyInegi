00b263774925f48f72ae8ec5c65163618fb75354
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2024-10-23 15:29:12 -0600

    derechos reservados

--- pyInegi/geografia.py
+++ pyInegi/geografia.py
@@ -1,5 +1,9 @@
 def generalizar(func=""):
-	from .reducePuntos import inicio_rp
-	from .separaLineas import inicio_sl
-	obj = {"reducePuntos":inicio_rp,"separaLineas":inicio_sl}
+	def _reduceP():
+		from .reducePuntos import inicio_rp
+		return inicio_rp
+	def _separaL():
+		from .separaLineas import inicio_sl
+		return inicio_sl
+	obj = {"reducePuntos":_reduceP(),"separaLineas":_separaL(),"ayuda":"Aqui va algunas presiciones del uso de esta libreria","desc":"Esta libreria fue desarrollada por el departamento de Generalización adscrito a la subdireccion de Edición Digital del INEGI. CopyRight INEGI@2024"}
 	return obj[func]
