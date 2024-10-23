6fd2d80b21c544c8aaacbea28475d5be64e51b1b
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2024-10-23 13:56:00 -0600

    reconstruccion de salidas en el init

--- pyInegi/__init__.py
+++ pyInegi/__init__.py
@@ -1,4 +1,10 @@
-from .generalizar import *
+import generalizar as gen
+
+class Generalizar:
+    def reducePuntos(**_d):
+        gen.reducePuntos(gdb=_d.gdb, feat=_d.feat, camp=_d.camp.split(","),dist=_d.dist,ver=_d.ver)
+    def separaLineas(_gdb,_fCls,distancia,cpu=cpu_count(),vtx=0,eps=0.001,apx=0.02):
+        gen.separaLineas(*[_gdb,_fCls,distancia,cpu,vtx,eps,apx])
 
 __version__="1.0.0"
 __ALL__ = ["generalizar","basico","reducePuntos","separaLineas"]
--- pyInegi/generalizar.py
+++ pyInegi/generalizar.py
@@ -1,8 +1,4 @@
 from .reducePuntos import reducePuntos
 from .separaLineas import separaLineas
-
-
-
-
-#from .clase1.basico import *
-#print(punto(5,9))
+from .clase1.basico import *
+print(punto(5,9))
