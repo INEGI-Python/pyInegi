8c53770e4657881b4c7bbeee0d688f2520e8c2b2
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2024-11-14 09:31:53 -0600

    webMap

--- pyInegi/webMap.py
+++ pyInegi/webMap.py
@@ -3,8 +3,8 @@ import folium as fol
 
 if __name__ == "__main__":
 	parser = ag.ArgumentParser(description="Genera un Mapa Web de las capas de datos que el usuario cargue. Soporta ShapeFile, FeatureClass, GeoJson ")
-	parser.add_argument('datos',type=str, help="Ruta absoluta o relativa  de la fuente de Datos")
-	parser.add_argument('tipo',type=str,  nargs='?', choices=["POINT","LINESTRING","POLYGON","MULTILINESTRING","MULTIPOLYGON"], help="Tipo de geometria. POINT, LINESTRING,POLYGON,MULTILINESTRING,MULTIPOLYGON")
-	parser.add_argument("capas",type=str, help="Ruta de los datos separados por comas")
+	parser.add_argument('datos',type=str, help="Ruta absoluta o relativa  de las fuentes de Datos sepradas por comas")
+	parser.add_argument('tipos',type=str,  choices=["POINT","LINESTRING","POLYGON","MULTILINESTRING","MULTIPOLYGON"], help="Tipo de geometria. POINT, LINESTRING,POLYGON,MULTILINESTRING,MULTIPOLYGON, separadas por  comas respectivamente con las  fuentes de datos")
+	parser.add_argument("props",type=str, nargs='?', default="{'name':'Layer1','color':'red','tooltips':True,'popups':True}", help="Objetos separados por comas con  las propiedades principales de cada Layer. Defualt: {'name':'Layer1','color':'red','tooltips':True,'popups':True} ")
 	args = parser.parse_args()
 	print(args)
