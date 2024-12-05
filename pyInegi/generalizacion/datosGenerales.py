a2f1776317179c0ace10853ff42fb352bad77c24
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2024-12-05 14:47:23 -0600

    print _dat

--- pyInegi/generalizacion/datosGenerales.py
+++ pyInegi/generalizacion/datosGenerales.py
@@ -4,12 +4,14 @@ def formato(msj, res, may=False):
 	return {"sucesses":True,"message":a} if res=="json" else a
 
 def ayuda(_dat):
-	a = """ Aqui va la super ayuda de como usar la libreria usar las lineas que le sean posibles"""                     
+	a = """ Aqui va la super ayuda de como usar la libreria usar las lineas que le sean posibles"""
+	print(_dat.values()[1:])
 	return formato(a,*_dat.values()[1:])
 
 def descripcion(_dat):
 	a ="""pyInegi es una libreria de código abierto para PYTHON desarrollada por el Instituto Nacional de Estadísitica y Geografía. INEGI. La cual cuadyuba a los ciudadanos 
 	mexicanos asi como al resto del mundo y sus alrededores a el proceda  CopyRight   INEGI@2024 """
+	print(_dat.values()[1:])
 	return formato(a,*_dat.values()[1:])
 
 def inicio(**datos):
