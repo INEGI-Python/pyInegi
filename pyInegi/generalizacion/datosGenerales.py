f80ee6bc0cbab4364c4c386c8153f3f083a87983
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2024-12-05 12:40:20 -0600

    upper

--- pyInegi/generalizacion/datosGenerales.py
+++ pyInegi/generalizacion/datosGenerales.py
@@ -1,7 +1,7 @@
 
 def ayuda(_dat):
 	a = """ Aqui va la super ayuda de como usar la libreria usar las lineas que le sean posibles"""                     
-	a1 = upper(a) if _dat["mayusculas"] else a
+	a1 = a.upper() if _dat["mayusculas"] else a
 	a2 = {"sucesses":True,"message":a1} if _dat['res']=="json" else a1 
 	return a2
 
