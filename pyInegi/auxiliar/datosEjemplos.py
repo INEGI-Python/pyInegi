b258f527eb9f5dd2170365f7f772d06a1fd7de54
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2025-09-29 12:33:46 -0600

    rutas en datosEjemplos

--- pyInegi/auxiliar/datosEjemplos.py
+++ pyInegi/auxiliar/datosEjemplos.py
@@ -1,11 +1,15 @@
 
 class paramMzas:
-   def __init__(self,file,dist,rows=-1):
+   def __init__(self,file,dist,out,rows=-1,prev=0):
       self.file=file
       self.dist=dist
+      self.out=out
       self.rows=rows
+      self.prev=prev
 
 def shapes(dato):
-   return {"mzas": "https://github.com/INEGI-Python/pyInegi/raw/41e30bb0f3c568d5ff981560a18aeeafac77c039/datosEjemplo/manzan.shp",
+   return {"mzas": "https://github.com/INEGI-Python/pyInegi/raw/d248d3a43c65915c7a3e64396cd6da57c65ad093/datosEjemplo/cartografiaUrbana.gdb/manzanas",
            "costa-acapulco": "https://github.com/INEGI-Python/pyInegi/raw/41e30bb0f3c568d5ff981560a18aeeafac77c039/datosEjemplo/osta-acapulco.shp"
    }[dato]
+
+
