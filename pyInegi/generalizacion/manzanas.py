268ff5985bfa1dff91111957a40ee15e120f3044
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2025-10-02 12:48:28 -0600

    Se corrigieron puntos colineales, se agregaron los campos y sus valores al resultado. y la distancias de algunos casos

--- pyInegi/generalizacion/manzanas.py
+++ pyInegi/generalizacion/manzanas.py
@@ -1,3 +1,4 @@
+from operator import ge
 import os
 import argparse
 import geopandas as geo
@@ -34,14 +35,23 @@ def quitaPrivadas(a):
 	gdb =a.file[0:len(a.file)-len(fuente[-1])-1]
 	print(f"Geodatabase: {gdb}")
 	gdf = geo.read_file(gdb,layer=fuente[-1],rows=a.rows if a.rows>0 else None)
+	gdf["OBJECTID"] = gdf.index + 1
+	gdf.set_index("OBJECTID",inplace=True)
 	gdf.loc[:, "geometry"] = gdf.geometry.buffer(0)
-	print(gdf.index)
+	campos = [c for c in gdf.columns if c not in ["geometry","objectid"]]
 	print("Elimina ángulos colineales de las geometrías de la capa a procesar...")
 	gdf["geometry"] = gdf["geometry"].apply(remove_colinear_points)
+	print("Generalizando polígonos...")
 	poligonos = [generaTriangulos_lineasFuera(gdf.loc[i,"geometry"],a.dist,gdf.crs,i) for i in gdf.index]
 	print("Proceso finalizado. Tiempo: %.3f seg" % (t()-ini))
 	print(f"Guardando resultados en la geodatabase...")
 	poligonos_gdf = geo.GeoDataFrame(geometry=[poly for sublist in poligonos for poly in sublist], crs=gdf.crs)
+	for col in campos:
+		poligonos_gdf[col] = None
+	for idx, polys in enumerate(poligonos):
+		for poly in polys:
+			for col in campos:
+				poligonos_gdf.loc[poligonos_gdf.geometry == poly, col] = gdf.iloc[idx][col]
 	feat(gdb,poligonos_gdf,a.out)
 	if a.prev==1:
 		import matplotlib
--- pyInegi/generalizacion/puntosColineares.py
+++ pyInegi/generalizacion/puntosColineares.py
@@ -5,14 +5,16 @@ def remove_colinear_points(geom, tolerance=5):
 		if geom.geom_type == "Polygon":
 			coords = list(geom.exterior.coords)
 			new_coords = [coords[0]]
-			for i in range(1, len(coords) - 1):
+			for i in range(1, len(coords) -1):
 				p0 = np.array(new_coords[-1])
 				p1 = np.array(coords[i])
 				p2 = np.array(coords[i + 1])
 				v1 = p1 - p0
 				v2 = p2 - p1
 				cross = np.cross(v1, v2)
-				if np.abs(cross) > tolerance:
+				angle = np.arctan2(np.linalg.norm(cross), np.dot(v1, v2))
+				angle_deg = np.degrees(angle)
+				if np.abs(angle_deg) > tolerance:
 					new_coords.append(coords[i])
 			new_coords.append(coords[-1])
 			return type(geom)(new_coords)
