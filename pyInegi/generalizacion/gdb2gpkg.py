97a70039cb9ed8413464892d5128576b8b113a37
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2025-10-15 10:19:33 -0600

    gdb2gpkg final

--- pyInegi/generalizacion/gdb2gpkg.py
+++ pyInegi/generalizacion/gdb2gpkg.py
@@ -1,26 +1,26 @@
+from platform import processor
 import os,shutil
-import geopandas as geo
 import   pandas as pan
 import argparse
 import fiona
-import rasterio as ras
 
 def gdb2gpkg(gdb_path, gpks_path,cond,tif):
-	if cond is not None:
-		tmp = gpks_path.replace("mapa_base.gpkg","batimetriasombreado.gpkg")
-		print(tif,tmp)
-		os.system(f'"C:\\Program Files\\QGIS 3.40.10\\bin\\gdal_translate.exe" -of GPKG -b 3 {tif} {tmp}')
-		os.system(f"REN  {tmp}  {gpks_path}")
+	if os.path.exists(gpks_path):
+		os.remove(gpks_path)
 
 	layers = fiona.listlayers(gdb_path)
 	for layer in layers:
-		gdf = geo.read_file(gdb_path, layer=layer)
-		gdf.to_file(gpks_path, layer=layer, driver="GPKG")
-	print(bati)
-	if os.path.exists(bati):
-		img = ras.open(bati,mode="r",driver="GTiff")
-		ras.open(gpks_path,mode="w+",layer="batimetriasombreado",driver="GPKG",width=img.width,height=img.height,count=img.count,dtype=type(img),crs=img.crs,transform=img.transform).write(img.read(),indexes=1)
-
+		with fiona.open(gdb_path,'r',layer=layer) as capaOrigen:
+			perfil = capaOrigen.profile
+			perfil['driver']='GPKG'
+			with fiona.open(gpks_path,'w',**perfil,layer=layer) as  capaDest:
+				for elemento in capaOrigen:
+					capaDest.write(elemento)
+	print(f"Capas  copiadas con exito a {gpks_path.split('/')[-2]}" )
+	if cond is not None:
+		print("... agregando el raster de batimetria...")
+		os.system(f'C:/Progra~1/QGIS34~1.10/bin/gdal_translate.exe -a_srs EPSG:4326  -of GPKG -co APPEND_SUBDATASET=YES -co RASTER_TABLE="batimetriasombreado"  {tif} {gpks_path}')
+	
 def copy_without_gdb(src, dst,tmpl):
 	excel = pan.read_excel(f"{tmpl}/Caneva_Estados.xlsx",index_col=0)
 	excel.sort_index(inplace=True)
@@ -38,9 +38,11 @@ def copy_without_gdb(src, dst,tmpl):
 			orient = str(excel[excel['NOMGEO']==ent]["Orientacion"].values[:])[2:-2]
 			print(f"{ent}  -  {orient}")
 			shutil.copy2(f"{tmpl}/{orient}.qgz",f"{dst_dir}/{ent}.qgz")
+			shutil.copy2(f"{root}/INEGI_Logotipo_5.jpg",f"{dst_dir}/INEGI_Logotipo_5.jpg")
 			bati = [f for f in files if f.endswith(".tif")]
 			bati = bati[0] if len(bati)>0 else None
 			gdb2gpkg(gdb,gpk,bati,f"{root}/{bati}")
+			os.system(f'{dst_dir}/{ent}.qgz')
 		dirs[:] = [d for d in dirs if not d.lower().endswith('.gdb')]
 
 
@@ -59,6 +61,5 @@ if __name__ == "__main__":
 # esquema={'geometry':'Point','properties':{'name':'str','value':'int'}}
 # feats=[{'geometry':{'type':'Point','coordinates':(10.0,20.0)},'properties': { 'name':'Elemento1','value':1000}},{'geometry':{'type':'Point','coordinates':(20.0,30.0)},'properties': { 'name':'Elemento2','value':2000}}]
 # myData = crearGPKG(esquema,'EPSG:4326',"miGeopaquete.gpkg",feats)
-
 
 
