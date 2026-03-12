10153851298d6a45759523c25aea38b58d4425a9
EMMANUEL.RODRIGUEZ <emmanuel.rodriguez@inegi.org.mx>
2026-03-12 11:43:08 -0600

    init

--- pyInegi/basico/__init__.py
+++ pyInegi/basico/__init__.py
@@ -1,3 +1,8 @@
 from .funciones import *
 
+
+__all__ = ['convertir', 'funciones']
+__version__ = '0.1.0'
+__author__ = 'INEGI@2025'    
+    
 data = {'dist':120.0,'EPSILON':0.001000,'APROX':0.020000}
